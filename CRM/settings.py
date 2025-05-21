99e5f9f81fd5f0fd46143816a92bd7e42c973636
DanielIlinskii <Chelovechek098@gmail.com>
2025-05-21 20:51:22 +0300

    создание простого логера через middleware

--- CRM/settings.py
+++ CRM/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "users.middleware.LoggerMiddleware",
 ]
 
 ROOT_URLCONF = "CRM.urls"
